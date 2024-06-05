de121cfe299c26efe76516452dba4d03474bec80
DilanQuiros1 <quiroschinchilladilan@gmail.com>
2024-06-05 07:11:23 -0600

    Version Inicial del proyecto con registro de departments en proceso

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { EmployeeRegistrationComponent } from './pages/employee-registration/employee-registration.component';
+
+@NgModule({
+  declarations: [AppComponent, EmployeeRegistrationComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/app/pages/employee-registration/employee-registration.component.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-registration',
+  templateUrl: './employee-registration.component.html',
+  styleUrl: './employee-registration.component.css',
+})
+export class EmployeeRegistrationComponent implements OnInit {
+  departments: any[] = [];
+
+  constructor(private http: HttpClient) {
+    this.LoadDepartments();
+  }
+  ngOnInit(): void {}
+
+  LoadDepartments() {
+    this.http.get('assets/departments.json').subscribe((res: any) => {
+      debugger;
+      this.departments = res.data;
+    });
+  }
+}
