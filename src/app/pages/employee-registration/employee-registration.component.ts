5c74f102cde3f67a6b5d7ff47e0f365c8f0447da
DilanQuiros1 <quiroschinchilladilan@gmail.com>
2024-06-08 17:33:36 -0600

    Se creo el metodo update y delete para usuarios

--- src/app/pages/employee-registration/employee-registration.component.ts
+++ src/app/pages/employee-registration/employee-registration.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-employee-registration',
@@ -11,6 +12,7 @@ export class EmployeeRegistrationComponent implements OnInit {
   departments: any[] = [];
   employee: any[] = [];
   isListView: boolean = true;
+  isEditing: boolean = false;
 
   employeeObject = {
     id: '',
@@ -22,11 +24,35 @@ export class EmployeeRegistrationComponent implements OnInit {
     phoneNumber: '',
   };
 
-  constructor(private http: HttpClient, private userService: UserService) {
-    this.LoadEmployees();
+  editButton(id: string) {
+    this.isListView = !this.isListView;
+    this.isEditing = true;
+    this.userService.getUserById(id).subscribe((res: any) => {
+      this.employeeObject = res;
+    });
+  }
+
+  toggleButton() {
+    this.isListView = !this.isListView;
+    this.isEditing = false;
+  }
+
+  constructor(
+    private http: HttpClient,
+    private userService: UserService,
+    private route: ActivatedRoute
+  ) {
+    //this.LoadEmployees();
     this.getUsers();
   }
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.userService.getUserById(id).subscribe((res: any) => {
+        this.employeeObject = res;
+      });
+    }
+  }
 
   LoadEmployees() {
     this.http.get('assets/getEmployee.json').subscribe((res: any) => {
@@ -43,16 +69,34 @@ export class EmployeeRegistrationComponent implements OnInit {
 
   getUsers() {
     this.userService.getUsers().subscribe((res: any) => {
-      this.employee = res;
+      debugger;
+      this.employee = res; // con archivos .json no usar res.data;
     });
   }
   onSubmit1() {
     console.log(this.employeeObject.email);
   }
 
-  onEdit(item: any) {
-    debugger;
-    this.employeeObject = item;
-    this.isListView = false;
+  onEdit() {
+    this.userService.updateUser(this.employeeObject).subscribe(
+      (res: any) => {
+        console.log('Update success');
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  deleteUser(id: string) {
+    this.userService.deleteById(id).subscribe(
+      (res: any) => {
+        console.log('deleted');
+        this.getUsers();
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -17,4 +17,16 @@ export class UserService {
   getUsers(): Observable<any> {
     return this.http.get<any>(this.apiUrl);
   }
+
+  updateUser(employee: any): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/${employee.id}`, employee);
+  }
+
+  getUserById(id: string): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/${id}`);
+  }
+
+  deleteById(id: string): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
 }
